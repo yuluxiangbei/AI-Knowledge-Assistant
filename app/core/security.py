83f1c0221580866d6d85c44441ea8d6a229b8a0f
yuluxiangbei <3077728049@qq.com>
2026-08-01 19:36:09 +0800

    feat: bcrypt 密码哈希 + JWT 签发与验证

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,9 @@
-import bcrypt
+from datetime import datetime, timedelta, timezone   # 标准库
 
+import bcrypt                                        # 第三方
+import jwt
+
+from app.core.config import get_settings             # 本地
 def hash_password(plain : str) -> str:
   passwd = bcrypt.hashpw(plain.encode("utf-8"),bcrypt.gensalt())
   return passwd.decode()
@@ -7,4 +11,23 @@ def hash_password(plain : str) -> str:
 def verify_password(plain : str, hashed: str) -> bool:
   result = bcrypt.checkpw(plain.encode("utf-8"),hashed.encode("utf-8"))
   return result
+
+settings = get_settings()
+def create_access_token(subject: str | int) -> str:
+
+  exp = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+  payload = {
+    "sub":str(subject),
+    "exp": exp,
+  }
+
+  token = jwt.encode(payload,settings.SECRET_KEY,algorithm=settings.ALGORITHM)
+  return token
+
+def decode_access_token(token: str) -> dict:
+  payload = jwt.decode(token,settings.SECRET_KEY,algorithms=[settings.ALGORITHM])
+  return payload
+  
+
 
